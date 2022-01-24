57e0622848f4461acc6b299cbc318a7cd38c5c01
miftah1991 <miftah.amin1991@gmail.com>
2022-01-25 00:05:33 +0430

    Add menu javascript

---
+++ menu.js
@@ -0,0 +1,21 @@
+const menubtn = document.querySelector('#menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closemenu = document.querySelector('.close-menu');
+const mobileMenuItems1 =document.querySelector('.mobile-menu-item1');
+const mobileMenuItems2 =document.querySelector('.mobile-menu-item2');
+const mobileMenuItems3 =document.querySelector('.mobile-menu-item3');
+
+function showMenue(e)
+{
+    e.preventDefault();
+    mobileMenu.style.display ='block';
+}
+function hideMenu(){
+    mobileMenu.style.display ='none';
+}
+
+menubtn.addEventListener('click',showMenue);
+closemenu.addEventListener('click',hideMenu);
+mobileMenuItems1.addEventListener('click',hideMenu);
+mobileMenuItems2.addEventListener('click',hideMenu);
+mobileMenuItems3.addEventListener('click',hideMenu);
