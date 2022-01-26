a1ce0b5bfdd6667357ab8b1ece74d6d0fc7eb1f7
miftah1991 <miftah.amin1991@gmail.com>
2022-01-26 22:49:17 +0430

    CSS Style for error message and javascript prevent default

--- validation.js
+++ validation.js
@@ -4,15 +4,16 @@ const emailForm = form.querySelector('#email');
 const regex = /[A-Z]/
 
 const message = document.createElement('p');
+message.className = 'error-message';
 message.innerText = 'Please submit your email in lower case' 
 
 function validate(a) {
   return regex.test(a);
 }
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
   if (validate(emailForm.value)){
   form.insertBefore(message, form.children[1]);
-  form.querySelector('button').preventDefault;
+  e.preventDefault();
   }
 });
