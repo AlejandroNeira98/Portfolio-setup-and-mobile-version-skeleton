4f1345c94e5362f57b8aed181b24d35e165235d1
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-01-26 13:37:42 -0500

    Linters

--- validation.js
+++ validation.js
@@ -1,19 +1,19 @@
 const form = document.querySelector('form');
 const emailForm = form.querySelector('#email');
 
-const regex = /[A-Z]/
+const regex = /[A-Z]/;
 
 const message = document.createElement('p');
 message.className = 'error-message';
-message.innerText = 'Please submit your email in lower case' 
+message.innerText = 'Please submit your email in lower case';
 
 function validate(a) {
   return regex.test(a);
 }
 
 form.addEventListener('submit', (e) => {
-  if (validate(emailForm.value)){
-  form.insertBefore(message, form.children[1]);
-  e.preventDefault();
+  if (validate(emailForm.value)) {
+    form.insertBefore(message, form.children[1]);
+    e.preventDefault();
   }
 });
