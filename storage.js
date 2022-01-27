aa54bf1af71881fdad1fad3f12dd5c226f43ed7f
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-01-27 11:34:55 -0500

    Add storage.js that retrieves and storages Object Form

---
+++ storage.js
@@ -0,0 +1,15 @@
+const form1 = document.querySelector('form');
+const fullname = form1.querySelector('#full-name');
+const email = form1.querySelector('#email');
+const message1 = form1.querySelector('#message');
+
+var formOb = {};
+
+function setData(txt, mail, msg) {
+  formOb.text = txt;
+  formOb.email = mail;
+  formOb.message = msg;
+  localStorage.setItem ('formData', formOb);
+}
+
+form1.addEventListener('keyup', (e) => {e.preventDefault(); setData(fullname.value, email.value, message1.value)});
